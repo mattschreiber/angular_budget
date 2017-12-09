0faae8d8cfdb13f7626c47c6412fd525fdb19c8a
mattschreiber <matt.schreiber01@gmail.com>
2017-12-09 09:43:25 -0500

    changed to https

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -1,5 +1,6 @@
 import { Config } from './config';
 
 export const CONFIGS: Config = {
-  baseUrl: 'http://matt-budget.us-east-2.elasticbeanstalk.com/'
+  baseUrl: 'https://matt-budget.us-east-2.elasticbeanstalk.com/'
+  // baseUrl: 'http://localhost:5000/'
 }
