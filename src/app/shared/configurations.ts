06c1d22c93fe4bbd115750b701812eb76739f5e7
mattschreiber <matt.schreiber01@gmail.com>
2017-12-11 09:05:40 -0500

    added progress spinner for first page load

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -1,6 +1,6 @@
 import { Config } from './config';
 
 export const CONFIGS: Config = {
-  baseUrl: 'https://matt-budget.us-east-2.elasticbeanstalk.com/'
-  // baseUrl: 'http://localhost:5000/'
+  // baseUrl: 'https://matt-budget.us-east-2.elasticbeanstalk.com/'
+  baseUrl: 'http://localhost:5000/'
 }
