9872030f16fb476bc259948dfeb3362936954b8b
mattschreiber <matt.schreiber01@gmail.com>
2017-12-17 09:46:17 -0500

    added sidenav

---
+++ src/app/services/sidenav.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+
+@Injectable()
+export class SidenavService {
+  private sidenav: MatSidenav;
+  constructor() { }
+
+  public setSidenav(sidenav: MatSidenav) {
+   this.sidenav = sidenav;
+ }
+
+ public sidenavToggle() {
+   this.sidenav.toggle();
+ }
+
+}
