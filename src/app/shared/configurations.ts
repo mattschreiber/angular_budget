8a74ed60ca9fa635ded40fe503791ff2b3244295
mattschreiber <matt.schreiber01@gmail.com>
2017-12-18 17:23:17 -0500

    update container

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -1,6 +1,6 @@
 import { Config } from './config';
 
 export const CONFIGS: Config = {
-  // baseUrl: 'https://www.matt-budget.me/'
-  baseUrl: 'http://localhost:5000/'
+  baseUrl: 'https://www.matt-budget.me/'
+  // baseUrl: 'http://localhost:5000/'
 }
