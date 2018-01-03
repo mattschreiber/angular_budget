ec90fe219b68662092c4489dd975f520669fe9c0
mattschreiber <matt.schreiber01@gmail.com>
2018-01-03 15:35:20 -0500

    installed angular4-fusion charts

--- src/app/reports/category-chart/category-chart.component.ts
+++ src/app/reports/category-chart/category-chart.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryChartComponent implements OnInit {
 
-  constructor() { }
+  id = 'chart1';
+  width = 600;
+  height = 400;
+  type = 'column2d';
+  dataFormat = 'json';
+  dataSource;
+  title = 'Angular4 FusionCharts Sample';
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.dataSource = {
+            "chart": {
+                "caption": "Harry's SuperMart",
+                "subCaption": "Top 5 stores in last month by revenue",
+                "numberprefix": "$",
+                "theme": "fint"
+            },
+            "data": [
+                {
+                    "label": "Bakersfield Central",
+                    "value": "880000"
+                },
+                {
+                    "label": "Garden Groove harbour",
+                    "value": "730000"
+                },
+                {
+                    "label": "Los Angeles Topanga",
+                    "value": "590000"
+                },
+                {
+                    "label": "Compton-Rancho Dom",
+                    "value": "520000"
+                },
+                {
+                    "label": "Daly City Serramonte",
+                    "value": "330000"
+                }
+            ]
+        }
   }
 
 }
--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -1,4 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { HttpModule } from '@angular/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FintTheme from 'fusioncharts/themes/fusioncharts.theme.fint';
+import { FusionChartsModule } from 'angular4-fusioncharts';
 import { CommonModule } from '@angular/common';
 
 import {MatGridListModule} from '@angular/material/grid-list';
@@ -6,9 +14,15 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { CategoryChartComponent } from './category-chart/category-chart.component';
 
+import 'hammerjs';
+
+FusionChartsModule.fcRoot(FusionCharts, Charts, FintTheme);
+
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    FusionChartsModule,
     MatGridListModule,
     ReportsRoutingModule
   ],
