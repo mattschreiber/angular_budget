559d20686dc0eec050c6dffe3215f30f8d6bd650
Matt <matt.schreiber01@gmail.com>
2019-03-21 21:16:37 -0400

    updates to make run locally

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -2,5 +2,5 @@ import { Config } from './config';
 
 export const CONFIGS: Config = {
   // baseUrl: 'https://www.matt-budget.me/'
-  baseUrl: 'http://localhost:5000/'
+  baseUrl: 'http://192.168.50.46:5000/'
 }
