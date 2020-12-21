9e13b069ad332ff1e8854d4b6b7886f7d5313436
mattschreiber <matt.schreiber01@gmail.com>
2020-12-21 11:36:08 -0500

    changed ip to local computer for dev

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -2,5 +2,6 @@ import { Config } from './config';
 
 export const CONFIGS: Config = {
   // baseUrl: 'https://www.matt-budget.me/'
-  baseUrl: 'http://192.168.50.46:5000/'
+  // baseUrl: 'http://192.168.50.46:5000/'
+  baseUrl: 'http://127.0.0.1:5000/'
 };
