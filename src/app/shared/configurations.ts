ce66e14f8cf04a063a9ad093fee082988554e756
mattschreiber <matt.schreiber01@gmail.com>
2021-01-10 11:06:29 -0500

    fixed layout

--- src/app/shared/configurations.ts
+++ src/app/shared/configurations.ts
@@ -1,7 +1,7 @@
 import { Config } from './config';
 
 export const CONFIGS: Config = {
-  baseUrl: 'https://www.matt-budget.me/'
+  // baseUrl: 'https://www.matt-budget.me/'
   // baseUrl: 'http://192.168.50.46:5000/'
-  // baseUrl: 'http://127.0.0.1:5000/'
+  baseUrl: 'http://127.0.0.1:5000/'
 };
